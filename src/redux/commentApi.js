3df5ab86d86ce3c0243836acf203721bf757dd75
LosOleksandr <losalexander2003@gmail.com>
2023-08-20 15:01:28 +0300

    add post

--- src/redux/commentApi.js
+++ src/redux/commentApi.js
@@ -1,20 +1,28 @@
-import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
+import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 
-const API_ENDPOINT = '/comments';
-const BASE_URL = 'https://64674adeba7110b663b466b2.mockapi.io/';
+const API_ENDPOINT = "/comments";
+const BASE_URL = "https://64674adeba7110b663b466b2.mockapi.io/";
 
 export const commentApi = createApi({
-  reducerPath: 'comments',
+  reducerPath: "comments",
   baseQuery: fetchBaseQuery({
     baseUrl: BASE_URL,
   }),
-  tagTypes: ['Comments'],
+  tagTypes: ["Comments"],
   endpoints: (builder) => ({
     getComments: builder.query({
       query: () => API_ENDPOINT,
-      providesTags: ['Comments'],
+      providesTags: ["Comments"],
+    }),
+    addComment: builder.mutation({
+      query: (newComment) => ({
+        url: API_ENDPOINT,
+        method: "POST",
+        body: newComment,
+      }),
+      invalidatesTags: ["Comments"],
     }),
   }),
 });
 
-export const { useGetCommentsQuery } = commentApi;
+export const { useGetCommentsQuery, useAddCommentMutation} = commentApi;
